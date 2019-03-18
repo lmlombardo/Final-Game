d78dfe7994f1145c4e59d540fd54a664b221fec0
luislombardis <anguilaelectrica3@gmail.com>
2019-03-18 03:36:32 +0100

    iniciado juego para primer hito

---
+++ Final game/Assets/Scripts/CheckGround.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckGround : MonoBehaviour {
+
+    private controles player;
+    
+
+
+	// Use this for initialization
+	void Start () {
+
+        player = GetComponentInParent<controles>();
+	}
+
+     void OnCollisionStay(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = true;
+        }
+    }
+
+     void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            player.grounded = false;
+        }   
+    }
+}
---
+++ Final game/Assets/Scripts/controles.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controles : MonoBehaviour {
+
+    
+    public float speed = 6f;
+    public float jumpPower = 6f;
+    public bool grounded;
+    
+
+    
+    private bool jumping = false;
+    private Rigidbody rb2d;
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        
+        rb2d = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float horizontal = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            if (grounded == true)
+                rb2d.velocity = new Vector2(rb2d.velocity.x, jumpPower);
+
+        }
+        
+    }
+}
